1cd7437e6fe4bb7b526895815af6608be648b96c
Joluzut” <luukvanzutphen39@gmail.com>
2023-09-26 16:38:43 +0200

    mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    qDebug() << "Hello World!";
+    qDebug() << "Hello JOEY STINKT NAAR GRAS";
 }
 
 MainWindow::~MainWindow()
